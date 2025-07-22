e1ac0e8b7c873e39e04894b10dc97e5343c218b8
Nathan Ansel <nathan@livefront.com>
2025-07-22 13:53:35 -0500

    Moved viewDidDisappear

--- Duvet/SheetViewController.swift
+++ Duvet/SheetViewController.swift
@@ -67,11 +67,6 @@ public class SheetViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
-    public override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        backgroundDimmingAnimator?.stopAnimation(true)
-    }
-
     // MARK: UIViewController
 
     public override func viewDidLoad() {
@@ -104,6 +99,11 @@ public class SheetViewController: UIViewController {
         if let sheetItem = sheetItems.last, sheetItem.viewController.parent != self {
             transitionSheet(fromSheetItem: nil, toSheetItem: sheetItem, forward: true, animated: false)
         }
+    }
+
+    public override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        backgroundDimmingAnimator?.stopAnimation(true)
     }
 
     // MARK: Push/Pop Sheets
